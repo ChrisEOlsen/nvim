133f8b5a7d7a3f776a0d5ce1176fa3538684e734
Chris Olsen <chrisevjenolsen@gmail.com>
2026-04-17 15:36:25 -0400

    feat(models): wire <leader>ac to model picker

--- lua/ai.lua
+++ lua/ai.lua
@@ -585,6 +585,10 @@ vim.keymap.set("n", "<leader>ah", function()
     require("history").open_panel(bufnr)
 end, { noremap = true, silent = true, desc = "AI History for current file" })
 
+vim.keymap.set("n", "<leader>ac", function()
+    require("models").open_picker()
+end, { noremap = true, silent = true, desc = "AI: pick model from favorites" })
+
 function M.save_config()
     save_ai_config()
 end
